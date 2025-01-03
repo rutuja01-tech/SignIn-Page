0c946541500dcca9c7a6d25517efa5b752da5c0c
rutuja01-tech <rutujakshirsagar001@gmail.com>
2025-01-03 16:22:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let inputElement = document.getElementById("inputElement");
+let signInTextElement = document.getElementById("signInText");
+
+function signIn() {
+    let inputValue = inputElement.value ;
+    let verifyText = "Hi "+ inputValue + ", verifying your account....";
+    signInTextElement.textContent =  verifyText;
+
+}
